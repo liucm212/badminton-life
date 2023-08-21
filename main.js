6235c474d17a4130cefddacd9ff3bc0069be6ff0
liucm212 <andyliu02120926@gmail.com>
2023-08-21 20:41:41 +0800

    新增賽事資訊頁面

--- main.js
+++ main.js
@@ -2,3 +2,9 @@
 import 'bootstrap/js/dist/modal';
 // all.scss
 import './assets/scss/all.scss';
+
+// js files for pages.
+import './js/navbar'
+import './js/event'
+import './js/calendar'
+import './js/dropdown'
